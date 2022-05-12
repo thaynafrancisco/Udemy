dd2a2f2f43a63a38f0d2961fae0d6d8c6b52dabf
Thayná Francisco <thaynacosta.f@outlook.com>
2022-05-11 22:54:19 -0300

    udemy-course

---
+++ Basico I/index.js
@@ -0,0 +1,40 @@
+// let cor = 'verde';
+
+// let cor = 'amarelo';
+
+
+// if (cor === 'verde') {
+
+//     console.log('siga')
+
+// } else if (cor === 'amarelo') {
+
+//     console.log('atenção')
+
+// } else if (cor === 'vermelho') {
+
+//     console.log('pare')
+
+// }
+
+
+// switch
+let cor = 'a';
+
+switch (cor) {
+    case 'verde':
+        console.log('siga');
+        break;
+
+    case 'amarelo':
+        console.log('atenção');
+        break;
+
+    case 'vermelho':
+        console.log('pare');
+        break;
+
+    default:
+        console.log("dont know")
+
+}
---
+++ Basico II/index.js
@@ -0,0 +1,91 @@
+// function calc(x1, x2, operator) {
+
+//     return eval(`${x1} ${operator} ${x2}`);
+
+// }
+
+// let resultado = calc(1, 2, "+");
+
+// console.log(resultado);
+
+// arrow function 
+
+// let calc = (x1, x2, operator) => {
+
+//     return eval(`${x1} ${operator} ${x2}`);
+
+// }
+
+// let resultado = calc(3, 2, "+");
+
+// console.log(resultado);
+
+// escuta
+
+// window.addEventListener ('focus', event =>{
+
+//     console.log("focus");
+
+// });
+
+// document.addEventListener('click', event =>{
+
+//     console.log('click');
+
+// });
+
+// get
+
+// let agora=new Date();
+
+// console.log(agora.toLocaleDateString("pt-BR"));
+
+// array
+
+// let carros = ["palio 98", "toro", "uno", 10, true, new Date(), function(){}];
+
+// console.log(carros)
+
+
+// console.log(carros.length)
+
+// console.log(carros[5].getFullYear())
+
+// foreach
+
+// carros.forEach(function(value, index){
+
+//     console.log(index, value);  
+                                
+// });
+
+
+
+// thayna array
+
+// let techs = ['html', 'css', 'js', 'figma', 'git', 'angular', 'react', 'firebase', 1]
+
+// console.log(techs)
+
+// techs.forEach(function(value, index){
+//     console.log(index, value)
+// })
+
+// classes, métodos, etc
+
+// class celular {
+//     constructor (){
+
+//         this.cor="prata";
+//     }
+
+//     ligar(){
+//         console.log("uma ligação");
+//         return "ligando";
+//     }
+// }
+
+// let objeto=new celular();
+
+// console.log(objeto);
+// console.log(objeto.ligar);
